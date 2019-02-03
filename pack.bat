467d3676adda1a3a693f52d25fe2220d4e7e3fd9
Παλαιολόγος <marekszuwarek1958@gmail.com>
2019-02-03 10:54:12 +0100

    Better project structure.
    Executable icon seems to be f***ed up, but toolbar is working properly.

--- pack.bat
+++ pack.bat
@@ -1,4 +1,4 @@
-@echo off
+
 echo -----------------------------------
 echo             P R O T O N            
 echo -----------------------------------
@@ -7,32 +7,36 @@ echo Copyright (C) 2019, licensed under
 echo terms of GPLv3 license.
 echo.
 
-mkdir tmp 2> nul
+mkdir dist
+mkdir tmp
 
 if "%1"=="" (
-    echo Usage: proton file.swf [icon.ico]
     goto end
 )
 
-copy %1 tmp\app.swf > nul
-copy data\projector.exe tmp\projector.exe > nul
+copy %1 tmp\app.swf
+copy data\projector.exe tmp\projector.exe
+copy data\title.exe tmp\title.exe
+copy config.ini tmp\config.ini
 
 if not "%2"=="" (
     tools\rcedit tmp\projector.exe --set-icon %2 2>nul
 )
 
-tools\7z a tmp\arc.7z tmp\app.swf tmp\projector.exe
+cd tmp
+..\tools\7z a arc.7z app.swf title.exe projector.exe config.ini
+cd..
 
 copy /b data\x86-mod.sfx + data\header.txt + tmp\arc.7z tmp\app.exe
 
 if not "%2"=="" (
     tools\rh -open tmp\app.exe -save tmp\new.exe -action addoverwrite -res %2 -mask ICONGROUP,MAINICON,
-    del tmp\app.exe 2> nul
-    copy tmp\new.exe tmp\app.exe 2> nul
-    del tmp\new.exe 2> nul
+    del tmp\app.exe
+    copy tmp\new.exe tmp\app.exe
+    del tmp\new.exe
 )
 
-copy tmp\app.exe app.exe
+copy tmp\app.exe dist\app.exe
 
 cd tmp
 del app.exe 2> nul
@@ -41,6 +45,8 @@ del app.swf 2> nul
 del arc.7z 2> nul
 del icon.ico 2> nul
 del projector.exe 2> nul
+del title.exe 2> nul
+del config.ini 2> nul
 cd ..
 
 echo Done!
